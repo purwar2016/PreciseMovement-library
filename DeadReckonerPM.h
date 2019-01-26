f31324d342f3957d44c735b37f8416c2ce534104
jaean123 <whileloops@gmail.com>
2019-01-26 00:26:32 -0500

    Twisting method changed.
    Twisting method changed.
    PID is done for individual wheels instead of using moveWheels method. 
    This gave better results.

--- DeadReckonerPM.h
+++ DeadReckonerPM.h
@@ -1,54 +1,104 @@
-
-#ifndef _DeadReckonerPM_h
-#define _DeadReckonerPM_h
+#ifndef _PreciseMover_h
+#define _PreciseMover_h
 
 #include <Arduino.h>
+#include "DeadReckonerPM.h"
+#include "PidPM.h"
 
-#define UNSIGNED_LONG_MAX 4294967295
-#define WHEEL_FORWARD 1
-#define WHEEL_REVERSE -1
+typedef void(*functiontype)();
 
-class DeadReckonerPM {
+class PreciseMover {
 
 public:
-	DeadReckonerPM(volatile unsigned long *, volatile unsigned long *, int, double, double);
-	DeadReckonerPM();
-	void construct(volatile unsigned long *, volatile unsigned long *, int, double, double);
-	void computePosition();
-	void setX(double);
-	void setY(double);
-	void setTheta(double);
-	void setLeftOmegaDirection(int);
-	void setRightOmegaDirection(int);
-	int getLeftOmegaDirection();
-	int getRightOmegaDirection();
-	double getX();
-	double getY();
-	double getW();
-	double getWl();
-	double getWr();
-	double getTheta();
-	void reset();
+    PreciseMover(
+            volatile unsigned long *newPulsesLeft,
+            volatile unsigned long *newPulsesRight,
+            int newPulsesPerRev,
+            int newEnA,
+            int newEnB,
+            double newLength,
+            double newRadius,
+            int newMinMotorSpeed,
+            int newTargetForwardSpeed,
+            double newStopLength,
+            int targetTwistOmega,
+            int angleErrorThreshold,
+            functiontype newMotorSetForward,
+            functiontype newMotorSetCCW,
+            functiontype newMotorSetCW,
+            functiontype motorBrake,
+            double newKpFW,
+            double newKiFW,
+            double newKpTW,
+            double newKiTW);
+
+    void forward(double);
+    void twist(double);
+
+    void setTuneDelay(int);
+
+    void tuneTwistPID();
+    void debug();
 
 private:
-	void computeAngularVelocities();
-	unsigned long getChange(unsigned long, unsigned long);
-	volatile unsigned long *leftTicks, *rightTicks; // Number of total wheel encoder tick counts for left and right wheels.
-	unsigned long leftTicksPrev, rightTicksPrev; // Number of total wheel encoder tick counts at time computeAngularVelocities() is called.
-	double xc, yc; // Latest position coordinates in ticks.
-	double wl, wr; // Latest left and right angular velocity of the wheels in radians per second.
-	int ticksPerRev; // Number of tick registers per second of the encoder.
-	float w; // Angular velocity of the robot.
-	double length; // Length from left wheel to right wheel.
-	double radius; // Radius of the wheel.
-	double theta;
-	double toRadPerSec; // ticks/microsecond to rad/s conversion factor
-	unsigned long prevIntegrationTime;
-	unsigned long prevWheelComputeTime;
-	int leftOmegaDirection = 1;
-	int rightOmegaDirection = 1;
-	unsigned long dt_omega;
-	unsigned long dt_integration;
+    volatile unsigned long *pulsesLeft, *pulsesRight;
+    double LENGTH;
+    double RADIUS;
+    double MIN_MOTOR_SPEED; // in PWM
+    int PULSES_PER_REV;
+
+    double ENA;
+    double ENB;
+
+    double PID_SAMPLE_TIME = 15;
+    double PID_MIN_FW = -100;
+    double PID_MAX_FW = 100;
+    double kpFW;
+    double kiFW;
+    double kdFW;
+    double inputFW;
+    double outputFW;
+    double setpointFW;
+    double PID_MIN_TW = 100;
+    double PID_MAX_TW = 255;
+    double kpTW;
+    double kiTW;
+    double kdTW;
+	double inputLeftTW, inputRightTW;
+	double outputLeftTW, outputRightTW;
+	double setpointLeftTW, setpointRightTW;
+
+    double TARGET_FORWARD_SPEED;
+    double TARGET_TWIST_MOTOR_OMEGA;
+
+    double ANGLE_ERROR_THRESHOLD;
+    double STOP_LENGTH;
+    double LOOK_AHEAD = 100;
+
+    unsigned long POSITION_COMPUTE_INTERVAL = 25; // ms
+    unsigned long CORRECTION_TIME_INTERVAL_FW = 50;
+    unsigned long CORRECTION_TIME_INTERVAL_TW = 25;
+
+    unsigned long prevPositionComputeTime = 0;
+    unsigned long prevCorrectionTime = 0;
+
+    functiontype motorSetForward;
+    functiontype motorSetCCW;
+    functiontype motorSetCW;
+    functiontype motorBrake;
+
+    DeadReckonerPM deadReckoner;
+    PidPM pidFW;
+	PidPM pidLeftMotorTW;
+	PidPM pidRightMotorTW;
+
+    void doDeadReckoning();
+    double getGoalPointX(double xPos, double yPos, double xTarget);
+    void moveWheels(int motorSpeed, double diff);
+
+    void printPlot(int actual, int target);
+
+    unsigned long tuneDelay = 1000; // ms
 };
 
 #endif
